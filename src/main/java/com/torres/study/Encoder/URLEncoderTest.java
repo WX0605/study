004420fe95b1d635ffc6842e3027d4ed0bd92555
wangxiao <wangxiao@aerozhonghuan.com>
2018-08-14 18:36:46 +0800

    java中文encoder

---
+++ src/main/java/com/torres/study/Encoder/URLEncoderTest.java
@@ -0,0 +1,15 @@
+package com.torres.study.Encoder;
+
+import java.net.URLEncoder;
+
+/**
+ * @author torres
+ * @Description: TODO
+ * @date 2018/8/13
+ */
+public class URLEncoderTest {
+
+    public static void main(String[] args) {
+        System.out.println("宁CFA915 encode="+URLEncoder.encode("宁CFA911"));
+    }
+}
